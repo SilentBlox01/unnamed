e42b5c7f51173e357a53b58e7478e9e3a944babd
SilentBlox01 <84942345+SilentBlox01@users.noreply.github.com>
2022-04-11 17:30:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const express = require('express')
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Your Bots Is Online!!!'))
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
 
 "$TOEKN"
 // if you need help ask in the help channel dont dm me
